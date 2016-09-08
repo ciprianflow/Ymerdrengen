276377f41d2283b2677cd9edb98874da7d4f9245
Jiri Korinek <kormaister@gmail.com>
2016-09-08 11:23:06 +0200

    hitlercopped the script

--- Ymerdrengen/Assets/Scripts/YoghurtDetection.cs
+++ Ymerdrengen/Assets/Scripts/YoghurtDetection.cs
@@ -1,10 +1,23 @@
-﻿using UnityEngine;
+﻿// <copyright file="YoghurtDetection.cs" company="Team4">
+// Company copyright tag.
+// </copyright>
 using System.Collections;
+using UnityEngine;
 
+/// <summary>
+/// Class for shitty yoghurt detection.
+/// </summary>
 public class YoghurtDetection : MonoBehaviour
 {
+    /// <summary>
+    /// Can move output variable.
+    /// </summary>
     public bool CanMove = true;
 
+    /// <summary>
+    /// Checking for collision with yoghurt.
+    /// </summary>
+    /// <param name="collision">Collision with objects in the scene</param>
     public void OnTriggerStay(Collider collision)
     {
         if (collision.gameObject.layer != 30)
