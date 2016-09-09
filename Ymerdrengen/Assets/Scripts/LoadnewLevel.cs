0e29585861f7329ecf13990685f491874819ad86
Ciprian Florescu <ciprianliis@gmail.com>
2016-09-09 13:50:15 +0200

    end level fix

--- Ymerdrengen/Assets/Scripts/LoadnewLevel.cs
+++ Ymerdrengen/Assets/Scripts/LoadnewLevel.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class LoadnewLevel : MonoBehaviour {
 
+    public GameObject Boy;
+
+    public float EndLevelDistance = 1f;
+
 	// Use this for initialization
 	void Start () {
         
@@ -10,6 +14,12 @@ public class LoadnewLevel : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	Application.LoadLevel(Application.loadedLevel + 1); 
+        
+        // distance until the girl
+        if (Vector3.Distance(Boy.transform.position, this.transform.position) < EndLevelDistance)
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+
 	}
 }
