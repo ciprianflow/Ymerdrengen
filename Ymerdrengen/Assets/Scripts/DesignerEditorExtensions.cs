36a4aea9033fdfe6c5b08dd28499c911ecceff82
Ciprian Florescu <ciprianliis@gmail.com>
2016-09-12 10:59:34 +0200

    Added .meta files to repo

---
+++ Ymerdrengen/Assets/Scripts/DesignerEditorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class DesignerEditorExtensions : Editor {
+    [MenuItem("Design Utilities/Start Debug Camera")]
+    public static void GetDebugCamera() {
+        GameObject gObj = new GameObject();
+        Camera debugCamera = gObj.AddComponent<Camera>();
+        //gObj.AddComponent<DebugCameraController>();
+
+        gObj.transform.eulerAngles = Camera.main.transform.eulerAngles;
+
+        debugCamera.depth = Camera.main.depth + 1;
+    }
+
+    [MenuItem("Design Utilities/Stop Debug Camera")]
+    public static void StopDebugCamera() {
+        var debugCamera = Camera.current;
+        if (debugCamera.tag == "MainCamera")
+            return;
+        else
+            Destroy(debugCamera.gameObject);
+    }
+}
---
+++ Ymerdrengen/Assets/Scripts/WwiseAudioScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class WwiseAudioScript : MonoBehaviour
+{
+    /// <summary>
+    /// Minimum music clip length in seconds
+    /// </summary>
+    public float MinMusicTime = 5f;
+
+    /// <summary>
+    /// Maximum music clip length in seconds
+    /// </summary>
+    public float MaxMusicTime = 10f;
+
+    /// <summary>
+    /// Minimum pause time in seconds
+    /// </summary>
+    public float MinPauseTime = 3f;
+
+    /// <summary>
+    /// Maximum pause time in seconds
+    /// </summary>
+    public float MaxPauseTime = 6f;
+
+    public void Start()
+    {
+        AkSoundEngine.PostEvent("start", gameObject);
+    }
+
+    void Update() 
+    {
+    }
+}
