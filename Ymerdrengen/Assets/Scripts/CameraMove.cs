e4323a1e6a38f2066487eb29e53c005f1e1b38a3
Angeliki Skandalou <agg.skandalou@gmail.com>
2016-09-12 14:37:52 +0200

    Camera Animation

---
+++ Ymerdrengen/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    public Animator CamAnimator;
+    public 
+    // Use this for initialization
+    void Start () {
+        CamAnimator = GetComponent<Animator>();
+        CamAnimator.SetTrigger("GameStarted");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (this.CamAnimator.GetCurrentAnimatorStateInfo(0).IsName("CameraStart"))
+        {
+            Debug.Log("camera move!");
+            CamAnimator.SetTrigger("CamIsMoving");
+        }
+
+
+    }
+}
