cfb63f9939d103275ce5286fd0fb0a40e7ca0f5f
Mari Kjærside <mabiscus@gmail.com>
2016-09-12 14:22:03 +0200

    Added basic Wwise functionality

--- Ymerdrengen/Assets/Scripts/DesignerEditorExtensions.cs
+++ Ymerdrengen/Assets/Scripts/DesignerEditorExtensions.cs
@@ -1,24 +1,40 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class DesignerEditorExtensions : Editor {
+/// <summary>
+/// Extensions to Unity to ease the workload on designers.
+/// </summary>
+public class DesignerEditorExtensions : Editor
+{
+    /// <summary>
+    /// Creates a new freeform camera and switches focus to that camera.
+    /// </summary>
     [MenuItem("Design Utilities/Start Debug Camera")]
-    public static void GetDebugCamera() {
-        GameObject gObj = new GameObject();
-        Camera debugCamera = gObj.AddComponent<Camera>();
-        //gObj.AddComponent<DebugCameraController>();
+    public static void GetDebugCamera()
+    {
+        GameObject gameObj = new GameObject();
+        Camera debugCamera = gameObj.AddComponent<Camera>();
+        // gObj.AddComponent<DebugCameraController>();
 
-        gObj.transform.eulerAngles = Camera.main.transform.eulerAngles;
+        gameObj.transform.eulerAngles = Camera.main.transform.eulerAngles;
 
         debugCamera.depth = Camera.main.depth + 1;
     }
 
+    /// <summary>
+    /// Destroys the freeform camera and returns to the main camera.
+    /// </summary>
     [MenuItem("Design Utilities/Stop Debug Camera")]
-    public static void StopDebugCamera() {
+    public static void StopDebugCamera()
+    {
         var debugCamera = Camera.current;
         if (debugCamera.tag == "MainCamera")
+        {
             return;
+        }
         else
-            Destroy(debugCamera.gameObject);
+        {
+            GameObject.Destroy(debugCamera.gameObject);
+        }
     }
 }
